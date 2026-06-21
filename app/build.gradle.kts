0068c487bdfe2b86a67f06cd355a80c77d712a51
Dannipuu <hermawanwildan78@gmail.com>
2026-06-21 19:48:07 +0700

    Add debug keystore and fix release build

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,13 +16,26 @@ android {
         versionName = "1.0"
     }
 
+    signingConfigs {
+        create("release") {
+            storeFile = file("debug.keystore")
+            storePassword = "android"
+            keyAlias = "androiddebugkey"
+            keyPassword = "android"
+        }
+    }
+
     buildTypes {
         release {
             isMinifyEnabled = false
+            signingConfig = signingConfigs.getByName("release")
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
             )
+        }
+        debug {
+            isMinifyEnabled = false
         }
     }
     compileOptions {
